66c364f44dc0120faee42abf38cd13ba5af28cd2
albertkjoller <albert_k_jacobsen@hotmail.com>
2023-09-18 09:40:06 +0200

    setup repository structure w. cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repository associated with assignments from the course 46765 Machine Learning for Signal Processing @ DTU (fall 2023)',
+    author='albertkjoller',
+    license='MIT',
+)
